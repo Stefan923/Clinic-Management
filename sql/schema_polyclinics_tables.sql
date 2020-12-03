3f819a4a94d793a7fbb3148d1071f0d082ada08f
Stefan923 <stefanpopescu923@gmail.com>
2020-12-03 14:55:34 +0200

    Schema tables rework

--- sql/schema_polyclinics_tables.sql
+++ sql/schema_polyclinics_tables.sql
@@ -1,161 +1,345 @@
-DROP DATABASE Policlinici;
-CREATE DATABASE IF NOT EXISTS Policlinici;
-USE Policlinici;
+-- -----------------------------------------------------
+-- Schema `polyclinics`
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `polyclinics`;
+USE `polyclinics`;
 
-CREATE TABLE IF NOT EXISTS specialitate(
-	id_specialitate VARCHAR(13) PRIMARY KEY,
-	denumire_specialitate VARCHAR(50)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`employees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`employees` (
+  `cnp` VARCHAR(13) NOT NULL,
+  `idMedicalUnit` INT NOT NULL,
+  `lastName` VARCHAR(25) NULL DEFAULT NULL,
+  `firstName` VARCHAR(50) NULL DEFAULT NULL,
+  `address` VARCHAR(100) NULL DEFAULT NULL,
+  `phoneNum` VARCHAR(10) NULL DEFAULT NULL,
+  `email` VARCHAR(25) NULL DEFAULT NULL,
+  `iban` VARCHAR(25) NULL DEFAULT NULL,
+  `contractNum` VARCHAR(25) NULL DEFAULT NULL,
+  `employmentDate` DATE NULL DEFAULT NULL,
+  `position` VARCHAR(25) NULL DEFAULT NULL,
+  `salary` FLOAT(10,2) NULL DEFAULT NULL,
+  `workedHrs` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`cnp`));
 
-CREATE TABLE IF NOT EXISTS acreditari_speciale(
-	id_acreditare VARCHAR(13) PRIMARY KEY,
-	denumire_acreditare VARCHAR(50)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`doctors`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`doctors` (
+  `cnpEmployee` VARCHAR(13) NOT NULL,
+  `rank` VARCHAR(20) NULL DEFAULT NULL,
+  `sealCode` VARCHAR(20) NULL DEFAULT NULL,
+  `scientificTitle` VARCHAR(20) NULL DEFAULT NULL,
+  `didacticTitle` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`cnpEmployee`),
+  CONSTRAINT `fk_doctors_cnpEmployee`
+    FOREIGN KEY (`cnpEmployee`)
+    REFERENCES `polyclinics`.`employees` (`cnp`));
 
-CREATE TABLE IF NOT EXISTS servicii_medicale(
-	id_servicii VARCHAR(13) PRIMARY KEY,
-    specialitate VARCHAR(50),
-    pret FLOAT(5, 2),
-    durata INT,
-    necesitate_competenta VARCHAR(2)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`specialities`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`specialities` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS programari(
-	id_programare VARCHAR(13) PRIMARY KEY,
-    data_programare DATE
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`doctor_specialities`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`doctor_specialities` (
+  `cnpDoctor` VARCHAR(13) NOT NULL,
+  `idSpeciality` INT NOT NULL,
+  PRIMARY KEY (`cnpDoctor`, `idSpeciality`),
+  INDEX `idx_doctor_specialities_idSpeciality` (`idSpeciality` ASC) INVISIBLE,
+  CONSTRAINT `fk_doctor_specialities_cnpDoctor`
+    FOREIGN KEY (`cnpDoctor`)
+    REFERENCES `polyclinics`.`doctors` (`cnpEmployee`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_doctor_specialities_idSpeciality`
+    FOREIGN KEY (`idSpeciality`)
+    REFERENCES `polyclinics`.`specialities` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS fisa_medicala(
-	id_fisa_medicala VARCHAR(13) PRIMARY KEY,
-    CONSTRAINT fk_serviciu FOREIGN KEY (id_fisa_medicala)
-        REFERENCES servicii_medicale(id_servicii)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`medical_units`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`medical_units` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NULL DEFAULT NULL,
+  `address` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS pacienti(
-	CNP_pacient VARCHAR(13) PRIMARY KEY,
-    CONSTRAINT fk_fisa_medicala FOREIGN KEY (CNP_pacient)
-        REFERENCES fisa_medicala(id_fisa_medicala),
-	CONSTRAINT fk_programari_pacient FOREIGN KEY (CNP_pacient)
-        REFERENCES programari(id_programare)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`cabinets`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`cabinets` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `idMedicalUnit` INT NOT NULL,
+  `name` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_cabinets_idMedicalUnit` (`idMedicalUnit` ASC) INVISIBLE,
+  CONSTRAINT `fk_cabinets_idMedicalUnit`
+    FOREIGN KEY (`idMedicalUnit`)
+    REFERENCES `polyclinics`.`medical_units` (`id`));
 
-CREATE TABLE IF NOT EXISTS istoric_tranzactii(
-	id_tranzactie INT AUTO_INCREMENT PRIMARY KEY,
-    tipul VARCHAR(10),
-    data_tranzactie DATE,
-    suma FLOAT(10, 2),
-    platitor VARCHAR(50),
-    beneficiar VARCHAR(50)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`equipments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`equipments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS echipament_medical(
-	id_echipament VARCHAR(13) PRIMARY KEY,
-    tip_echipament VARCHAR(20)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`cabinet_equipments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`cabinet_equipments` (
+  `idCabinet` INT NOT NULL,
+  `idEquipment` INT NOT NULL,
+  PRIMARY KEY (`idCabinet`, `idEquipment`),
+  INDEX `fk_cabinet_equipments_idEquipment_idx` (`idEquipment` ASC) VISIBLE,
+  CONSTRAINT `fk_cabinet_equipments_idCabinet`
+    FOREIGN KEY (`idCabinet`)
+    REFERENCES `polyclinics`.`cabinets` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cabinet_equipments_idEquipment`
+    FOREIGN KEY (`idEquipment`)
+    REFERENCES `polyclinics`.`equipments` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS permisii_roluri(
-	id_permisie VARCHAR(13) PRIMARY KEY,
-	denumire_permisie VARCHAR(20)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`accreditations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`accreditations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS unitati_medicale(
-	id_unitati_medicale VARCHAR(13) PRIMARY KEY,
-    denumire VARCHAR(50),
-    adresa VARCHAR(100),
-    CONSTRAINT fk_specialitati FOREIGN KEY (id_unitati_medicale)
-        REFERENCES servicii_medicale (id_servicii)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`doctor_accreditations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`doctor_accreditations` (
+  `cnpDoctor` VARCHAR(13) NOT NULL,
+  `idAccreditation` INT NOT NULL,
+  PRIMARY KEY (`cnpDoctor`, `idAccreditation`),
+  CONSTRAINT `fk_doctor_accreditations_idAccreditation`
+    FOREIGN KEY (`idAccreditation`)
+    REFERENCES `polyclinics`.`accreditations` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_doctor_accreditations_idDoctor`
+    FOREIGN KEY (`cnpDoctor`)
+    REFERENCES `polyclinics`.`doctors` (`cnpEmployee`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS angajati(
-	CNP_angajat VARCHAR(13) PRIMARY KEY,
-    nume VARCHAR(25),
-    prenume VARCHAR(50),
-    adresa VARCHAR(100),
-    nr_telefpn VARCHAR(10),
-    email VARCHAR(25),
-    cont_IBAN VARCHAR(25),
-    nr_contract VARCHAR(25),
-    data_angajarii DATE,
-    functia VARCHAR(25),
-    salariu FLOAT(10, 2),
-    nr_ore_lucrate INT,
-    CONSTRAINT fk_policlinica FOREIGN KEY (CNP_angajat)
-        REFERENCES unitati_medicale (id_unitati_medicale)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`medical_unit_employees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`medical_unit_employees` (
+  `idMedicalUnit` INT NOT NULL,
+  `cnpEmployee` VARCHAR(13) NOT NULL,
+  PRIMARY KEY (`idMedicalUnit`, `cnpEmployee`),
+  INDEX `fk_medical_unit_employees_cnpEmployee_idx` (`cnpEmployee` ASC) VISIBLE,
+  CONSTRAINT `fk_medical_unit_employees_idMedicalUnit`
+    FOREIGN KEY (`idMedicalUnit`)
+    REFERENCES `polyclinics`.`medical_units` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_medical_unit_employees_cnpEmployee`
+    FOREIGN KEY (`cnpEmployee`)
+    REFERENCES `polyclinics`.`employees` (`cnp`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS roluri(
-	id_rol VARCHAR(13) PRIMARY KEY,
-    CONSTRAINT fk_roluri FOREIGN KEY (id_rol)
-        REFERENCES angajati (CNP_angajat),
-	denumire_rol VARCHAR(20),
-    CONSTRAINT fk_permisie FOREIGN KEY (id_rol)
-        REFERENCES permisii_roluri(id_permisie)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`nurse`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`nurse` (
+  `cnpEmployee` VARCHAR(13) NOT NULL,
+  `type` VARCHAR(20) NULL DEFAULT NULL,
+  `rank` VARCHAR(20) NULL,
+  PRIMARY KEY (`cnpEmployee`),
+  CONSTRAINT `fk_nurse_cnpEmployee`
+    FOREIGN KEY (`cnpEmployee`)
+    REFERENCES `polyclinics`.`employees` (`cnp`));
 
-CREATE TABLE IF NOT EXISTS conturi_aplicatie(
-	username VARCHAR(13) PRIMARY KEY,
-    parola VARCHAR(13),
-    CONSTRAINT fk_rol FOREIGN KEY (username)
-        REFERENCES roluri(id_rol)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`holidays`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`holidays` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cnp` VARCHAR(13) NOT NULL,
+  `startDate` DATE NOT NULL,
+  `endDate` DATE NOT NULL,
+  INDEX `fk_holidays_cnp_idx` (`cnp` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_holidays_cnpEmployee`
+    FOREIGN KEY (`cnp`)
+    REFERENCES `polyclinics`.`employees` (`cnp`));
 
-CREATE TABLE IF NOT EXISTS concedii(
-	id_concediu VARCHAR(13) PRIMARY KEY,
-    data_start DATE,
-    data_final DATE,
-    CONSTRAINT fk_concediu FOREIGN KEY (id_concediu)
-        REFERENCES angajati(CNP_angajat)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`medical_services`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`medical_services` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `idSpeciality` INT NOT NULL,
+  `idMedicalUnit` INT NOT NULL,
+  `hasAccreditation` TINYINT NULL DEFAULT 0,
+  `price` FLOAT(5,2) NULL DEFAULT NULL,
+  `duration` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_medical_services_idMedicalUnti_idx` (`idMedicalUnit` ASC) VISIBLE,
+  CONSTRAINT `fk_medical_services_idMedicalUnti`
+    FOREIGN KEY (`idMedicalUnit`)
+    REFERENCES `polyclinics`.`medical_units` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS program_de_lucru_angajat(
-	id_program VARCHAR(13) PRIMARY KEY,
-    CONSTRAINT fk_program_lucru FOREIGN KEY (id_program)
-        REFERENCES angajati (CNP_angajat),
-	ziua_saptamanii VARCHAR(10),
-    ora_start VARCHAR(5),
-    ora_final VARCHAR(5)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`patients`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`patients` (
+  `cnp` VARCHAR(13) NOT NULL,
+  PRIMARY KEY (`cnp`));
 
-CREATE TABLE IF NOT EXISTS asistenti(
-	CNP_asistent VARCHAR(13) PRIMARY KEY,
-    CONSTRAINT fk_asistent FOREIGN KEY (CNP_asistent)
-        REFERENCES angajati (CNP_angajat),
-	tipul VARCHAR(20),
-    gradul VARCHAR(20)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`patient_history`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`patient_history` (
+  `id` INT NOT NULL,
+  `cnpPatient` VARCHAR(13) NOT NULL,
+  `idService` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_patitent_history_idService_idx` (`idService` ASC) VISIBLE,
+  CONSTRAINT `fk_patitent_history_idService`
+    FOREIGN KEY (`idService`)
+    REFERENCES `polyclinics`.`medical_services` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_patient_history_cnpPatient`
+    FOREIGN KEY (`cnpPatient`)
+    REFERENCES `polyclinics`.`patients` (`cnp`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS medici(
-	CNP_medic VARCHAR(13) PRIMARY KEY,
-    CONSTRAINT fk_medic FOREIGN KEY (CNP_medic)
-        REFERENCES angajati (CNP_angajat),
-	CONSTRAINT fk_specialitate FOREIGN KEY (CNP_medic)
-        REFERENCES specialitate(id_specialitate),
-    gradul VARCHAR(20),
-    cod_parafa VARCHAR(20),
-    titlu_stiintific VARCHAR(20),
-    post_didactic VARCHAR(20),
-    CONSTRAINT fk_acreditare FOREIGN KEY (CNP_medic)
-        REFERENCES acreditari_speciale(id_acreditare),
-	CONSTRAINT fk_programari FOREIGN KEY (CNP_medic)
-        REFERENCES programari(id_programare)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`transactions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`transactions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(10) NOT NULL,
+  `date` DATE NULL,
+  `amount` FLOAT(10,2) NOT NULL,
+  `sender` VARCHAR(50) NOT NULL,
+  `receiver` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS program_functionare_policlinica(
-	id_program VARCHAR(13) PRIMARY KEY,
-    CONSTRAINT fk_program FOREIGN KEY (id_program)
-        REFERENCES unitati_medicale (id_unitati_medicale),
-	ziua_saptamanii VARCHAR(10),
-    ora_deschidere VARCHAR(5),
-    ora_inchidere VARCHAR(5)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`roles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS cabinet(
-	id_cabinet VARCHAR(13) PRIMARY KEY,
-	denumire_cabinet VARCHAR(50),
-    CONSTRAINT fk_id FOREIGN KEY (id_cabinet)
-        REFERENCES unitati_medicale (id_unitati_medicale),
-	CONSTRAINT fk_programari_cabinet FOREIGN KEY (id_cabinet)
-        REFERENCES programari(id_programare),
-	CONSTRAINT fk_echipament FOREIGN KEY (id_cabinet)
-        REFERENCES echipament_medical(id_echipament)
-);
+-- -----------------------------------------------------
+-- Table `polyclinics`.`accounts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`accounts` (
+  `cnpEmployee` VARCHAR(20) NOT NULL,
+  `username` VARCHAR(20) NOT NULL,
+  `idRole` INT NOT NULL DEFAULT 0,
+  `password` VARCHAR(45) NOT NULL,
+  INDEX `fk_accounts_cnpEmployee_idx` (`cnpEmployee` ASC) VISIBLE,
+  INDEX `fk_accounts_idRole_idx` (`idRole` ASC) VISIBLE,
+  CONSTRAINT `fk_accounts_cnpEmployee`
+    FOREIGN KEY (`cnpEmployee`)
+    REFERENCES `polyclinics`.`employees` (`cnp`),
+  CONSTRAINT `fk_accounts_idRole`
+    FOREIGN KEY (`idRole`)
+    REFERENCES `polyclinics`.`roles` (`id`));
+
+-- -----------------------------------------------------
+-- Table `polyclinics`.`role_permissions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`role_permissions` (
+  `idRole` INT NOT NULL,
+  `permission` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idRole`, `permission`),
+  CONSTRAINT `fk_role_permissions_idRole`
+    FOREIGN KEY (`idRole`)
+    REFERENCES `polyclinics`.`roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+-- -----------------------------------------------------
+-- Table `polyclinics`.`employee_schedule`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`employee_schedule` (
+  `cnpEmployee` VARCHAR(13) NOT NULL,
+  `idMedicalUnit` INT NOT NULL,
+  `dayOfWeek` VARCHAR(10) NOT NULL,
+  `startHour` VARCHAR(5) NOT NULL,
+  `endHour` VARCHAR(5) NOT NULL,
+  PRIMARY KEY (`cnpEmployee`),
+  INDEX `fk_employee_schedule_idMedicalUnit_idx` (`idMedicalUnit` ASC) VISIBLE,
+  CONSTRAINT `fk_employee_schedule_cnpEmployee`
+    FOREIGN KEY (`cnpEmployee`)
+    REFERENCES `polyclinics`.`employees` (`cnp`),
+  CONSTRAINT `fk_employee_schedule_idMedicalUnit`
+    FOREIGN KEY (`idMedicalUnit`)
+    REFERENCES `polyclinics`.`medical_units` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+-- -----------------------------------------------------
+-- Table `polyclinics`.`medical_unit_schedule`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`medical_unit_schedule` (
+  `idMedicalUnit` INT NOT NULL,
+  `dayOfWeek` VARCHAR(10) NOT NULL,
+  `startHour` VARCHAR(5) NOT NULL,
+  `endHour` VARCHAR(5) NOT NULL,
+  PRIMARY KEY (`idMedicalUnit`, `dayOfWeek`),
+  CONSTRAINT `fk_medical_unit_schedule_idMedicalUnit`
+    FOREIGN KEY (`idMedicalUnit`)
+    REFERENCES `polyclinics`.`medical_units` (`id`));
+
+-- -----------------------------------------------------
+-- Table `polyclinics`.`appointments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `polyclinics`.`appointments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cnpPatient` VARCHAR(13) NOT NULL,
+  `cnpDoctor` VARCHAR(13) NOT NULL,
+  `idCabinet` INT NOT NULL,
+  `data_programare` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_appointments_idCabinet_idx` (`idCabinet` ASC) VISIBLE,
+  INDEX `fk_appointments_idPatient_idx` (`cnpPatient` ASC) VISIBLE,
+  INDEX `fk_appointments_cnpDoctor_idx` (`cnpDoctor` ASC) VISIBLE,
+  CONSTRAINT `fk_appointments_idCabinet`
+    FOREIGN KEY (`idCabinet`)
+    REFERENCES `polyclinics`.`cabinets` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_appointments_idPatient`
+    FOREIGN KEY (`cnpPatient`)
+    REFERENCES `polyclinics`.`patients` (`cnp`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_appointments_cnpDoctor`
+    FOREIGN KEY (`cnpDoctor`)
+    REFERENCES `polyclinics`.`doctors` (`cnpEmployee`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
