4f27950e31d894e4eaf281541fdd4b734ce9e8b9
Stefan923 <stefanpopescu923@gmail.com>
2020-12-11 23:01:21 +0200

    Added ON_EMPLOYEE_SCHEDULE_UPDATE trigger

--- sql/schema_polyclinics_triggers.sql
+++ sql/schema_polyclinics_triggers.sql
@@ -7,3 +7,13 @@ BEGIN
 	END IF;
 END;
 // DELIMITER ;
+
+DROP TRIGGER IF EXISTS ON_EMPLOYEE_SCHEDULE_UPDATE;
+DELIMITER //
+CREATE TRIGGER ON_EMPLOYEE_SCHEDULE_UPDATE BEFORE UPDATE ON `employee_schedule` FOR EACH ROW
+BEGIN
+	IF ((SELECT E.`position` FROM `employees` E WHERE E.`cnp`=OLD.`cnpEmployee`) LIKE 'Medic') THEN
+		DELETE FROM `appointments` A WHERE A.`cnpDoctor`=OLD.`cnpEmployee` AND DAYNAME(A.`date`)=OLD.`dayOfWeek` AND TIME(A.`date`)>=OLD.`startHour` AND TIME(A.`date`)<=OLD.`endHour` AND (TIME(A.`date`)<NEW.`startHour` OR TIME(A.`date`)>NEW.`endHour`);
+	END IF;
+END;
+// DELIMITER ;
