52e32c91b8a0bb20626825a987d3b56b66c3d768
Stefan923 <stefanpopescu923@gmail.com>
2020-12-11 23:00:54 +0200

    Added UPDATE_EMPLOYEE_SCHEDULE procedure

--- sql/schema_polyclinics_procedures.sql
+++ sql/schema_polyclinics_procedures.sql
@@ -47,14 +47,14 @@ END;
 
 DROP PROCEDURE IF EXISTS UPDATE_EMPLOYEE_SCHEDULE;
 DELIMITER //
-CREATE PROCEDURE UPDATE_EMPLOYEE_SCHEDULE(IN `_cnpEmployee` VARCHAR(13), IN `_idMedicalUnit` INT, IN `_dayOfWeek` VARCHAR(10), IN `_startHour` TIME, IN `_endHour` TIME, IN `force` INT, OUT `validation` INT)
+CREATE PROCEDURE UPDATE_EMPLOYEE_SCHEDULE(IN `_cnpEmployee` VARCHAR(13), IN `_idMedicalUnit` INT, IN `_dayOfWeek` VARCHAR(10), IN `_startHour` TIME, IN `_endHour` TIME, IN `new_startHour` TIME, IN `new_endHour` TIME, IN `force` INT, OUT `validation` INT)
 BEGIN
-	IF ((SELECT COUNT(*) FROM `employee_schedule` WHERE `cnpEmployee`=`_cnpEmployee` AND `idMedicalUnit`=`_idMedicalUnit` AND `dayOfWeek` LIKE `_dayOfWeek` AND `startHour`>=`_startHour` AND `endHour`<=`_endHour`) >= 1) THEN
+	IF ((SELECT COUNT(*) FROM `employee_schedule` WHERE `cnpEmployee`=`_cnpEmployee` AND `idMedicalUnit`=`_idMedicalUnit` AND `dayOfWeek` LIKE `_dayOfWeek` AND `startHour`=`_startHour` AND `endHour`=`_endHour`) = 1) THEN
 		IF ((SELECT `position` FROM `employees` WHERE `cnp`=`_cnpEmployee`) NOT LIKE 'Medic'
 			OR `force`='1'
 			OR(SELECT COUNT(*) FROM `appointments` WHERE `cnpDoctor`=`_cnpEmployee` AND DAYNAME(`date`) LIKE `_dayOfWeek` AND TIME(`date`)>=`_startHour` AND TIME(`date`)<=`_endHour`) = 0) THEN
             
-			DELETE FROM `employee_schedule` WHERE `cnpEmployee`=`_cnpEmployee` AND `idMedicalUnit`=`_idMedicalUnit` AND `dayOfWeek` LIKE `_dayOfWeek` AND `startHour`>=`_startHour` AND `endHour`<=`_endHour`;
+			UPDATE `employee_schedule` SET `startHour`=`new_startHour`, `endHour`=`new_endHour` WHERE `cnpEmployee`=`_cnpEmployee` AND `idMedicalUnit`=`_idMedicalUnit` AND `dayOfWeek` LIKE `_dayOfWeek` AND `startHour`=`_startHour` AND `endHour`=`_endHour`;
 			SET `validation` = 1; -- s-a executat
 		ELSE
 			SET `validation` = 0; -- sunt programari care trebuiesc sterse
@@ -76,16 +76,17 @@ BEGIN
 	-- CALL INSERT_EMPLOYEE_SCHEDULE('2700927417309', '3', 'Monday', '08:00:00', '12:00:00', @output);
 	-- CALL INSERT_EMPLOYEE_SCHEDULE('2700927417309', '3', 'Monday', '07:59:00', '09:00:00', @output);
 	-- CALL INSERT_EMPLOYEE_SCHEDULE('2700927417309', '3', 'Monday', '11:00:00', '11:00:01', @output);
-	CALL DELETE_EMPLOYEE_SCHEDULE('2700927417309', '3', 'Monday', '12:00:00', '18:00:00', 1, @output);
+	-- CALL DELETE_EMPLOYEE_SCHEDULE('2700927417309', '3', 'Monday', '12:00:00', '18:00:00', 1, @output);
+    CALL UPDATE_EMPLOYEE_SCHEDULE('2700927417309', '3', 'Monday', '13:00:00', '16:00:00', '14:55:00', '16:00:00', 1, @output);
     SELECT @output;
 END;
 // DELIMITER ;
-SELECT COUNT(*), TIME(`date`), DAYNAME(`date`) FROM `appointments` WHERE `cnpDoctor` LIKE '2700927417309' AND DAYNAME(`date`) LIKE 'Monday' AND TIME(`date`)>=TIME('10:00:01') AND TIME(`date`)<=TIME('17:00:00');
+
 CALL TEST();
 
 -- INGORA
 -- (TIME_TO_SEC(TIME(`date`)) + `duration` * 60)>=TIME_TO_SEC(`_startHour`) AND TIME(`date`)<=`_endHour`)
 
-SELECT * FROM `employees` WHERE `lastName`='?' AND `firstName`='?' AND `position`='?';
-SELECT * FROM `employee_schedule` WHERE `cnpEmployee`='?';
-SELECT * FROM `holidays` WHERE `cnpEmployee`='?';
+-- SELECT * FROM `employees` WHERE `lastName`='?' AND `firstName`='?' AND `position`='?';
+-- SELECT * FROM `employee_schedule` WHERE `cnpEmployee`='?';
+-- SELECT * FROM `holidays` WHERE `cnpEmployee`='?';
