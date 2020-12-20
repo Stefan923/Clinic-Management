85441aa1cbd1998e3447dae611a000b996881bc8
Stefan923 <stefanpopescu923@gmail.com>
2020-12-20 18:00:16 +0200

    Added Database class (for DB connection)

--- desktop-app/src/main/java/com/sanitas/clinicapp/ClinicApplication.java
+++ desktop-app/src/main/java/com/sanitas/clinicapp/ClinicApplication.java
@@ -1,11 +1,18 @@
 package com.sanitas.clinicapp;
 
-import com.sanitas.clinicapp.homepage.HomePageMVC;
+import com.sanitas.clinicapp.login.LoginMVC;
 
 public class ClinicApplication {
 
+    private static Database database;
+
     public static void main(String[] args) {
-        new HomePageMVC();
+        database = new Database("localhost", 3306, "polyclinics", "root", "DButcnMySQL");
+        new LoginMVC();
+    }
+
+    public static Database getDatabase() {
+        return database;
     }
 
 }
