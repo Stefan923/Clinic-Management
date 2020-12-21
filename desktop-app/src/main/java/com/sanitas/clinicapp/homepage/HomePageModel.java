749570c7be1ea88ab26d530a4a15bc311269e074
Stefan923 <stefanpopescu923@gmail.com>
2020-12-22 01:07:03 +0200

    Initializing MrView()

--- desktop-app/src/main/java/com/sanitas/clinicapp/homepage/HomePageModel.java
+++ desktop-app/src/main/java/com/sanitas/clinicapp/homepage/HomePageModel.java
@@ -1,10 +1,11 @@
 package com.sanitas.clinicapp.homepage;
 
 import com.sanitas.clinicapp.hr.HrMVC;
+import com.sanitas.clinicapp.mr.MrMVC;
 
 public class HomePageModel {
 
-    public void openMVC(int number) {
+    public void openMVC(int number, HomePageView view) {
         switch (number) {
             case 1:
                 new HrMVC();
@@ -12,6 +13,7 @@ public class HomePageModel {
             case 2:
                 break;
             case 3:
+                new MrMVC(view);
                 break;
             case 4:
                 break;
