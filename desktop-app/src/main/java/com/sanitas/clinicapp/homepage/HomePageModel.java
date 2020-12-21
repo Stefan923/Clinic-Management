0e204ee7b4a3a5a13d82e95bd9b05c29fc7e4a94
Stefan923 <stefanpopescu923@gmail.com>
2020-12-21 12:51:26 +0200

    Initializing HrMVC class

--- desktop-app/src/main/java/com/sanitas/clinicapp/homepage/HomePageModel.java
+++ desktop-app/src/main/java/com/sanitas/clinicapp/homepage/HomePageModel.java
@@ -1,10 +1,13 @@
 package com.sanitas.clinicapp.homepage;
 
+import com.sanitas.clinicapp.hr.HrMVC;
+
 public class HomePageModel {
 
     public void openMVC(int number) {
         switch (number) {
             case 1:
+                new HrMVC();
                 break;
             case 2:
                 break;
